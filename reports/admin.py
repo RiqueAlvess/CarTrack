e63bc08cb1e5fbd156789664e36da9b227600e72
RiqueAlvess <henrique.alves.siqueira@hotmail.com>
2025-08-06 23:51:52 -0600

    Add files via upload

--- reports/admin.py
+++ reports/admin.py
@@ -1,30 +1,100 @@
 from django.contrib import admin
-from .models import Relatorio
+from .models import Relatorio, ImagemRelatorio
 
+class ImagemRelatorioInline(admin.TabularInline):
+    model = ImagemRelatorio
+    extra = 1
+    readonly_fields = ('data_upload',)
 
 @admin.register(Relatorio)
 class RelatorioAdmin(admin.ModelAdmin):
-    list_display = ['id', 'empresa', 'usuario', 'total_cleaned', 'forecasted_drops', 'enviado_por_email', 'data_criacao']
-    list_filter = ['enviado_por_email', 'data_criacao', 'empresa']
-    search_fields = ['empresa__nome', 'usuario__username']
-    date_hierarchy = 'data_criacao'
-    readonly_fields = ['data_criacao', 'data_atualizacao']
+    list_display = (
+        'usuario',
+        'data_formatada',
+        'hora_formatada',
+        'total_carros',
+        'total_cleaned',
+        'forecasted_drops',
+        'email_enviado',
+        'enviar_por_email'
+    )
+    
+    list_filter = (
+        'email_enviado',
+        'enviar_por_email',
+        'data_criacao',
+        'usuario'
+    )
+    
+    search_fields = (
+        'usuario__email',
+        'usuario__first_name',
+        'usuario__last_name'
+    )
+    
+    readonly_fields = (
+        'data_criacao',
+        'data_atualizacao',
+        'total_carros'
+    )
     
     fieldsets = (
         ('Informações Básicas', {
-            'fields': ('empresa', 'usuario', 'enviado_por_email', 'data_envio')
+            'fields': (
+                'usuario',
+                'data_criacao',
+                'data_atualizacao'
+            )
         }),
         ('Dados do Relatório', {
             'fields': (
-                ('ready_line', 'vip_line'),
-                ('overflow_kiosk', 'overflow_2'),
-                ('black_top', 'return_line'),
-                ('mecanico', 'gas_run'),
-                ('total_cleaned', 'forecasted_drops'),
+                'ready_line',
+                'vip_line',
+                'overflow_kiosk',
+                'overflow_2',
+                'black_top',
+                'return_line',
+                'mecanico',
+                'gas_run',
+                'total_cleaned',
+                'forecasted_drops'
             )
         }),
-        ('Datas', {
-            'fields': ('data_criacao', 'data_atualizacao'),
-            'classes': ('collapse',)
+        ('Configurações de E-mail', {
+            'fields': (
+                'enviar_por_email',
+                'email_enviado',
+                'tentativas_envio',
+                'erro_envio'
+            )
         }),
     )
+    
+    inlines = [ImagemRelatorioInline]
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(usuario=request.user)
+
+@admin.register(ImagemRelatorio)
+class ImagemRelatorioAdmin(admin.ModelAdmin):
+    list_display = (
+        'relatorio',
+        'descricao',
+        'data_upload'
+    )
+    
+    list_filter = (
+        'data_upload',
+        'relatorio__usuario'
+    )
+    
+    readonly_fields = ('data_upload',)
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(relatorio__usuario=request.user)
--- reports/models.py
+++ reports/models.py
@@ -1,43 +1,178 @@
 from django.db import models
-from django.conf import settings
-from dashboard.models import Empresa
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 
+User = get_user_model()
 
 class Relatorio(models.Model):
-    # Referência à empresa
-    empresa = models.ForeignKey(
-        Empresa, 
-        on_delete=models.CASCADE, 
-        related_name='relatorios_reports'
+    # Relacionamento com usuário
+    usuario = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='relatorios',
+        verbose_name='Usuário'
     )
     
-    # Informações básicas do relatório
-    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_atualizacao = models.DateTimeField(auto_now=True)
+    # Campo de texto simples para unidade/empresa
+    unidade_empresa = models.CharField(
+        max_length=200,
+        blank=True,
+        verbose_name='Unidade/Empresa',
+        help_text='Nome da unidade ou empresa'
+    )
     
-    # Campos do relatório (baseado nas imagens)
-    ready_line = models.CharField(max_length=10, blank=True, null=True)
-    vip_line = models.CharField(max_length=10, blank=True, null=True)
-    overflow_kiosk = models.CharField(max_length=10, blank=True, null=True)
-    overflow_2 = models.CharField(max_length=10, blank=True, null=True)
-    black_top = models.CharField(max_length=10, blank=True, null=True)
-    return_line = models.CharField(max_length=10, blank=True, null=True)
-    mecanico = models.CharField(max_length=10, blank=True, null=True)
-    gas_run = models.CharField(max_length=10, blank=True, null=True)
+    # Campos do relatório
+    ready_line = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Ready Line'
+    )
     
-    # Totais
-    total_cleaned = models.IntegerField(default=0)
-    forecasted_drops = models.IntegerField(default=0)
+    vip_line = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='VIP Line'
+    )
     
-    # Status de envio
-    enviado_por_email = models.BooleanField(default=False)
-    data_envio = models.DateTimeField(null=True, blank=True)
+    overflow_kiosk = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Overflow Kiosk'
+    )
+    
+    overflow_2 = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Overflow 2'
+    )
+    
+    black_top = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Black Top'
+    )
+    
+    return_line = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Return Line'
+    )
+    
+    mecanico = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Mecânico'
+    )
+    
+    gas_run = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Gas Run'
+    )
+    
+    total_cleaned = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Total de Carros Limpos'
+    )
+    
+    forecasted_drops = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0)],
+        verbose_name='Previsão de Drops'
+    )
+    
+    # Configurações de envio
+    enviar_por_email = models.BooleanField(
+        default=False,
+        verbose_name='Enviar por e-mail'
+    )
+    
+    # Status do envio
+    email_enviado = models.BooleanField(
+        default=False,
+        verbose_name='E-mail enviado'
+    )
+    
+    tentativas_envio = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Tentativas de envio'
+    )
+    
+    erro_envio = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Erro no envio'
+    )
+    
+    # Timestamps
+    data_criacao = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Data de criação'
+    )
+    
+    data_atualizacao = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Data de atualização'
+    )
     
     class Meta:
-        verbose_name = "Relatório"
-        verbose_name_plural = "Relatórios"
+        verbose_name = 'Relatório'
+        verbose_name_plural = 'Relatórios'
         ordering = ['-data_criacao']
-    
+        
     def __str__(self):
-        return f"Relatório {self.id} - {self.empresa.nome} - {self.data_criacao.strftime('%d/%m/%Y %H:%M')}"
+        return f"Relatório de {self.usuario.get_full_name() or self.usuario.username} - {self.data_criacao.strftime('%d/%m/%Y %H:%M')}"
+    
+    @property
+    def total_carros(self):
+        """Calcula o total de carros processados"""
+        return (
+            self.ready_line + self.vip_line + self.overflow_kiosk + 
+            self.overflow_2 + self.black_top + self.return_line + 
+            self.mecanico + self.gas_run
+        )
+    
+    @property
+    def data_formatada(self):
+        """Retorna a data formatada"""
+        return self.data_criacao.strftime('%d/%m/%Y')
+    
+    @property
+    def hora_formatada(self):
+        """Retorna a hora formatada"""
+        return self.data_criacao.strftime('%H:%M')
+
+
+class ImagemRelatorio(models.Model):
+    """Modelo para armazenar imagens dos relatórios"""
+    relatorio = models.ForeignKey(
+        Relatorio,
+        on_delete=models.CASCADE,
+        related_name='imagens',
+        verbose_name='Relatório'
+    )
+    
+    imagem = models.ImageField(
+        upload_to='relatorios/%Y/%m/%d/',
+        verbose_name='Imagem'
+    )
+    
+    descricao = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name='Descrição'
+    )
+    
+    data_upload = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Data do upload'
+    )
+    
+    class Meta:
+        verbose_name = 'Imagem do Relatório'
+        verbose_name_plural = 'Imagens dos Relatórios'
+        ordering = ['data_upload']
+        
+    def __str__(self):
+        return f"Imagem do {self.relatorio} - {self.data_upload.strftime('%d/%m/%Y %H:%M')}"
